d1227d1220d486a4edb4e52bca1b937f190b42f8
celina520524 <celina520524@163.com>
2021-09-14 15:55:36 +0800

    init: movie

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '@chakra-ui/theme'
+// import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+    {/* <CSSReset /> */}
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
 
 export default MyApp
